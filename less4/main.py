eb60214ac682305be2319aba96e572501b30b6f0
Misha <saba07022001@yandex.ru>
2022-09-29 20:22:37 +0300

    less4_files

--- less4/main.py
+++ less4/main.py
@@ -1,1 +1,23 @@
-# less4
+# # less4
+# import colorama 
+# print(colorama.Fore.YELLOW)
+# print('this is yellow !!!')
+# def bad_print(word):
+#   print(word)
+# def good_print(*args):
+#   print(args[0])
+# # bad_print('misha','python')
+# good_print('misha','python','=love')
+# def calculator(a,b,operation='+'):
+#   if operation=='+':
+#     c=a+b
+#     print(c)
+#   elif operation=='-':
+#     c=a-b
+#     print(c)
+# calculator(3,2)
+# print('a','b')
+path='less4/files/text.txt'
+file=open(path,'w')
+file.write('hello world!')
+file.close()
